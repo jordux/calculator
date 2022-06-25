91d1cd5c2411852c155b162cc60669c9f30107c9
jordux <nimmyislearningtoo@gmail.com>
2022-06-24 22:51:33 -0400

    finish calculator styling, add more JS variables'
    exit

--- calculator.js
+++ calculator.js
@@ -30,4 +30,27 @@ function operate(operator, a, b) {
     } 
 }
 
+// CLEAR BUTTON
+let clearButton = document.querySelector('.clearButton')
+clearButton.addEventListener('click', () => display.textContent = " ")
+clearButton.addEventListener('click', () => clearButton.textContent = " ")
+
+
+// SHOW BUTTON CLICKED ON DISPLAY
+let buttons = document.querySelectorAll('.button')
+let display = document.querySelector('.displayText')
+
+// VALUE ON DISPLAY STORED AS A displayValue variable
+                Array
+                    .from(buttons)
+                    .forEach(element => element.addEventListener('click', () => displayValue = display.textContent += `${element.textContent}`))
+
+
+// CURRENT OPERATOR STORED in currentOperator variable
+let operatorButton = document.querySelectorAll('.operator')
+    Array
+        .from(operatorButton)
+        .forEach(button => button.addEventListener('click', () => currentOperator = `${button.textContent}`))
+
+
 
