65835663f659d4632cfa56fd6548ede008e0340c
jordux <nimmyislearningtoo@gmail.com>
2022-06-29 22:27:54 -0400

    add complete calculator functionality

--- calculator.js
+++ calculator.js
@@ -1,56 +1,78 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator')
+const firstOperand = document.querySelector('[data-firstOperand]')
+const secondOperand = document.querySelector('[data-secondOperand]')
+const equalsButton = document.querySelector('[data-equals]')
+const clearButton = document.querySelector('[data-clear]')
+
+let currentOperator
+let num1
+let num2
+
+// MATH FUNCTIONS
 function addFunction(a, b) {
-        return a + b
+        return firstOperand.textContent = parseInt(a) + parseInt(b)
 }
 
 function subtractFunction (a, b) {
-        return a - b
+        return firstOperand.textContent = parseInt(a) - parseInt(b)
 }
 
 function multiplyFunction (a, b) {
-        return a * b
+        return firstOperand.textContent = parseInt(a) * parseInt(b)
 }
 
 function divideFunction (a, b) {
-        return a / b
+        return firstOperand.textContent = parseInt(a) / parseInt(b)
 }
 
-function operate(operator, a, b) {
 
-  if (operator === '+') {
+//OPERATE FUNCTION, CALLED WHEN EQUALS BUTTON IS HIT
+
+function operate(currentOperator, a, b) {
+
+  if (currentOperator === '+') {
             return addFunction(a, b)
 
-    } else if (operator === '-') {
+    } else if (currentOperator === '-') {
             return subtractFunction(a, b)
 
-    } else if (operator === '*') {
+    } else if (currentOperator === 'x') {
             return multiplyFunction(a, b)
 
-    } else if (operator === '/') {
+    } else if (currentOperator === '/') {
             return divideFunction(a, b)
     } 
 }
 
+// Show numbers on display
+
+numberButtons.forEach(button => {
+        button.addEventListener('click', () => {
+                firstOperand.innerText += button.innerText
+        });
+})
+
+operatorButtons.forEach(button => {
+        button.addEventListener('click', () => {
+                secondOperand.innerText = firstOperand.innerText  
+                num1 = secondOperand.innerText
+                currentOperator = button.innerText
+                firstOperand.innerText = " "
+
+        });
+})
+
+// EQUALS BUTTON
+equalsButton.addEventListener('click', () => {
+                num2 = firstOperand.innerText
+                operate(currentOperator, num1, num2)
+                secondOperand.innerText = " "
+        });
+
 // CLEAR BUTTON
-let clearButton = document.querySelector('.clearButton')
-clearButton.addEventListener('click', () => display.textContent = " ")
-clearButton.addEventListener('click', () => clearButton.textContent = " ")
-
-
-// SHOW BUTTON CLICKED ON DISPLAY
-let buttons = document.querySelectorAll('.button')
-let display = document.querySelector('.displayText')
-
-// VALUE ON DISPLAY STORED AS A displayValue variable
-                Array
-                    .from(buttons)
-                    .forEach(element => element.addEventListener('click', () => displayValue = display.textContent += `${element.textContent}`))
-
-
-// CURRENT OPERATOR STORED in currentOperator variable
-let operatorButton = document.querySelectorAll('.operator')
-    Array
-        .from(operatorButton)
-        .forEach(button => button.addEventListener('click', () => currentOperator = `${button.textContent}`))
-
-
-
+clearButton.addEventListener('click', () => {
+        firstOperand.innerText = ""
+        secondOperand.innerText = ""
+        currentOperator = undefined
+})
